2261beaec600acd98c60c9461a4baa57d78bf45c
HannaViel <annromsky@gmail.com>
2023-02-08 12:15:35 +0200

    upgrade

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ export default function App() {
     <div className="App">
       Weather App
       <footer>
-        This project was coded by Hanna Vielkova and is
+        This project was coded by Hanna Vielkova and is{" "}
         <a
-          href="https://github.com/HannaViel/weather-react-app"
+          href="https://github.com/HannaViel/final_react_project"
           target="_blank"
         >
           open-sourced on GitGub
