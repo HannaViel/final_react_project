3af9b4ea88823ce38f1f6b44edca90d93d06f903
HannaViel <annromsky@gmail.com>
2023-02-08 12:46:55 +0200

    updated coding info

--- src/App.js
+++ src/App.js
@@ -7,13 +7,29 @@ export default function App() {
     <div className="App">
       Weather App
       <footer>
-        This project was coded by Hanna Vielkova and is{" "}
+        This project was coded by{" "}
+        <a
+          href="https://preeminent-cajeta-8bd6e7.netlify.app/index.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hanna Vielkova
+        </a>{" "}
+        and is{" "}
         <a
           href="https://github.com/HannaViel/final_react_project"
           target="_blank"
           rel="noopener noreferrer"
         >
           open-sourced on GitGub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://visionary-gingersnap-cdd50a.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
         </a>
       </footer>
     </div>
