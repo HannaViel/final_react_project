ca85f5ad077710a58eb4b78ec32ab0ca7d5b7037
HannaViel <annromsky@gmail.com>
2023-02-08 12:00:00 +0200

    deleted unessery files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      Weather App
+      <footer>
+        This project was coded by Hanna Vielkova and is
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/HannaViel/weather-react-app"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          open-sourced on GitGub
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
